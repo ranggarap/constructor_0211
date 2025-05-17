73b6962c4acd273e85d977df8d20754839032633
ranggarap <fadhilahrangga26@gmail.com>
2025-05-17 18:02:14 +0700

    definisi function cons dan dest

--- destructor.cpp
+++ destructor.cpp
@@ -13,3 +13,36 @@ public:
     void cetakData();
     void isiData();
 };
+// Definisi member Functon
+angka::angka(int i)
+{ // Constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka()
+{ // Destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
